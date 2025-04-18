2f2f15124010793730e0e96e3613c2c2010d65b6
Ray <july021230@gmail.com>
2025-04-18 18:25:36 +0800

    feat(neovide)!: disable cursor animation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,13 +4,15 @@
 
 -- neovide
 if vim.g.neovide then
-  vim.g.neovide_title_text_color = "pink"
+  vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_floating_shadow = true
   vim.g.neovide_fullscreen = true
   vim.g.neovide_hide_mouse_when_typing = true
   vim.o.guifont = "JetBrainsMono Nerd Font:h13"
   -- vim.o.guifont = "CaskaydiaCove Nerd Font:h13"
   -- vim.o.guifont = "Hack Nerd Font:h13"
+
+  -- vim.g.neovide_title_text_color = "pink"
 end
 
 -- nvy
