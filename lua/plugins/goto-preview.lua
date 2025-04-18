52ad6ef9a03f0ce33969bcea918902a16656b274
Ray <july021230@gmail.com>
2025-04-18 18:22:00 +0800

    feat(goto-preview): add logger.nvim dependence

--- lua/plugins/goto-preview.lua
+++ lua/plugins/goto-preview.lua
@@ -1,22 +1,34 @@
-return {
+return
+-- {
+--   "rmagatti/goto-preview",
+--   event = "LazyFile",
+--   config = function()
+--     require("goto-preview").setup({
+--       default_mappings = true,
+--       preview_window_title = { enable = false, position = "left" },
+--       -- post_open_hook = function(buf, win)
+--       --   local orig_state = vim.api.nvim_buf_get_option(buf, "modifiable")
+--       --   vim.api.nvim_buf_set_option(buf, "modifiable", false)
+--       --   vim.api.nvim_create_autocmd({ "WinLeave" }, {
+--       --     buffer = buf,
+--       --     callback = function()
+--       --       vim.api.nvim_win_close(win, false)
+--       --       vim.api.nvim_buf_set_option(buf, "modifiable", orig_state)
+--       --       return true
+--       --     end,
+--       --   })
+--       -- end,
+--     })
+--   end,
+-- }
+{
   "rmagatti/goto-preview",
+  dependencies = { "rmagatti/logger.nvim" },
   event = "LazyFile",
   config = function()
     require("goto-preview").setup({
       default_mappings = true,
       preview_window_title = { enable = false, position = "left" },
-      -- post_open_hook = function(buf, win)
-      --   local orig_state = vim.api.nvim_buf_get_option(buf, "modifiable")
-      --   vim.api.nvim_buf_set_option(buf, "modifiable", false)
-      --   vim.api.nvim_create_autocmd({ "WinLeave" }, {
-      --     buffer = buf,
-      --     callback = function()
-      --       vim.api.nvim_win_close(win, false)
-      --       vim.api.nvim_buf_set_option(buf, "modifiable", orig_state)
-      --       return true
-      --     end,
-      --   })
-      -- end,
     })
   end,
 }
