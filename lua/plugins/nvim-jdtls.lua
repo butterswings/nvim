f7a0e3d02690e10cceac0612309fd233109f2da8
zhulei <zhulei26@xiaomi.com>
2025-09-04 02:45:01 +0800

    feat(java): add jdk settings for jdtls

---
+++ lua/plugins/nvim-jdtls.lua
@@ -0,0 +1,21 @@
+return {
+  "mfussenegger/nvim-jdtls",
+  opts = {
+    settings = {
+      java = {
+        configuration = {
+          runtimes = {
+            {
+              name = "jdk21",
+              path = "D:\\ScoopApp\\Scoop\\apps\\openjdk21\\current",
+            },
+            {
+              name = "jdk8",
+              path = "C:\\Users\\zhulei\\.jdks\\corretto-1.8.0_462",
+            },
+          },
+        },
+      },
+    },
+  },
+}
